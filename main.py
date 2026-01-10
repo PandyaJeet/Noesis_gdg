4a3ce1f94f0ff71fb23aee22c880d866d84afd9e
Om Prajapati <omaprajapati06@gmail.com>
2026-01-10 23:55:50 +0000

    remove

--- main.py
+++
@@ -1,8 +0,0 @@
-from firebase_functions import https_fn
-from flask import Flask
-from app import app as flask_app
-
-@https_fn.on_request()
-def app(req: https_fn.Request) -> https_fn.Response:
-    with flask_app.request_context(req.environ):
-        return flask_app.full_dispatch_request()
